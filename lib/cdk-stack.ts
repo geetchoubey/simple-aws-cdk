1a833f60aafb5e7c03ddd8f718470aefe7fc881c
Geet Choubey <geetchoubey@gmail.com>
2020-08-29 02:11:56 +0530

    Initial commit

--- lib/cdk-stack.ts
+++ lib/cdk-stack.ts
@@ -1,9 +1,10 @@
 import * as cdk from '@aws-cdk/core';
+import * as bucket from '@aws-cdk/aws-s3';
 
 export class CdkStack extends cdk.Stack {
   constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
-
     // The code that defines your stack goes here
+    new bucket.Bucket(this, 'SampleBucket');
   }
 }
